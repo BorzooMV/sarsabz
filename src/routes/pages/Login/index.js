f626cc8bcf6736807f415d8401402efe7ce95538
Borzoo Moazami <borzoomoazami94@gmail.com>
2022-02-03 09:28:52 +0330

    Style : Make login page responsive

--- src/routes/pages/Login/index.js
+++ src/routes/pages/Login/index.js
@@ -12,12 +12,26 @@ const useStyles = makeStyles(theme => ({
         left:"50%",
         transform:"translateX(-50%)",
         borderRadius:"5px",
+				[theme.breakpoints.down('md')]: {
+					width:'600px'
+				},
+				[theme.breakpoints.down('sm')]: {
+					width:"90%",
+					left:"50%",
+					top:"50%",
+					transform:"translate(-50%,-50%)",
+					
+				},
     },
     text: {
         display:"flex",
         alignItems:"center",
         flexDirection:"column",
-        justifyContent:"center"
+        justifyContent:"center",
+				[theme.breakpoints.down('sm')]: {
+					order:"2",
+					padding:"50px 0px"
+				}
     },
     divider: {
         margin:"10px auto",
@@ -36,13 +50,23 @@ const useStyles = makeStyles(theme => ({
 		imageContainer: {
 				height:"100%",
 				width:"400px",
-				padding:"0px"
+				padding:"0px",
+				[theme.breakpoints.down('md')]: {
+					width:'300px'
+				},
+				[theme.breakpoints.down('sm')]: {
+					width:"100%"
+				},
 		},
 		loginImage: {
 			  width: "100%",
 				height: "100%",
 				borderBottomLeftRadius: "5px",
 				borderTopLeftRadius: "5px",
+				[theme.breakpoints.down('sm')]: {
+					borderTopLeftRadius:"0px",
+					borderBottomLeftRadius:"0px"
+				}
 		},
 
 }))
@@ -52,7 +76,7 @@ const Login = () => {
     return(
         <Box className={classes.root} sx={{boxShadow : 3}} >
             <Grid container >
-                <Grid item xs={12} md={6} className={classes.text}>
+                <Grid item xs={12} sm={6} className={classes.text}>
                     <Stack direction="column">
                         <Typography variant="h4" component="h2">خوش آمدید</Typography>
                         <Typography variant="subtitle1" component="span">لطفا مشخصاتتون رو وارد کنید</Typography>
@@ -64,7 +88,7 @@ const Login = () => {
                         </form>
                     </Stack>
                 </Grid>
-                <Grid item xs={12} md={6}>
+                <Grid item xs={12} sm={6}>
                     <Box className={classes.imageContainer}>
                         <img className={classes.loginImage} src={loginVase} />
                     </Box>
