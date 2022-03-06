ba4da69058b40c49740dc9b01232335ac569dc70
Borzoo Moazami <borzoomoazami94@gmail.com>
2022-03-07 00:30:53 +0330

    Updates

---
+++ precache-manifest.29148103ca7fb234a6b036f132a8f2fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5117c7dabcc1226b6f4d8c7e9a476c4",
+    "url": "/sarsabz/index.html"
+  },
+  {
+    "revision": "57bc121411af55c10d44",
+    "url": "/sarsabz/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "770b09a8d7438fd4cac6",
+    "url": "/sarsabz/static/js/2.2cd08ee6.chunk.js"
+  },
+  {
+    "revision": "57bc121411af55c10d44",
+    "url": "/sarsabz/static/js/main.d1bbf299.chunk.js"
+  },
+  {
+    "revision": "8359a8898f0315a6caf8",
+    "url": "/sarsabz/static/js/runtime~main.11e37e62.js"
+  },
+  {
+    "revision": "2f515bb105a7d6e830d8f4eb654b5fdb",
+    "url": "/sarsabz/static/media/Chili.2f515bb1.jpg"
+  },
+  {
+    "revision": "7e110760931de01e3f2f71ea83d0ac52",
+    "url": "/sarsabz/static/media/loginVase.7e110760.jpg"
+  },
+  {
+    "revision": "e990e24fefc55afbb67d5aaae9347e51",
+    "url": "/sarsabz/static/media/vaseProfile.e990e24f.jpg"
+  }
+]);
